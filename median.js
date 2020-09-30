5dbfdfb566a9a6f724733bf72914708330be4bf5
Calvin Octa Wijaya <calvin_octa@windowslive.com>
2020-10-01 00:17:55 +0700

    Update median.js
    Finished , you can try now

--- median.js
+++ median.js
@@ -1,3 +1,5 @@
 function median(arr) {
   // you can only write your code here!
+  var midIndex = Math.floor((arr.length-1) /2);
+  return arr.length % 2 === 1 ? arr[midIndex] : ((arr[midIndex] + arr[midIndex+1])/2);
 }
