9725212d073c8ebc1647cfc9f40e1cdb15c469b3
Calvin Octa Wijaya <calvin_octa@windowslive.com>
2020-10-01 00:22:58 +0700

    Update reverseArray.js

--- reverseArray.js
+++ reverseArray.js
@@ -1,1 +1,8 @@
 // Make a reverse function with help from javascript array / string built-in function
+function reverseArray(arr) {
+    var result = [];
+    for(var i = arr.length-1; i >= 0; i--) {
+        result.push(arr[i]);
+    }
+    return result;
+}
